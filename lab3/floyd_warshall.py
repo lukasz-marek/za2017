5e87560c3c028c40424eb37449681bc22426eef6
Łukasz Marek <marek.lucas@gmail.com>
2017-10-17 13:21:34 +0200

    algorithm preview

--- lab3/floyd_warshall.py
+++ lab3/floyd_warshall.py
@@ -1,3 +1,5 @@
+import math
+
 def load_data(filepath):
     graph = {}
     with open(filepath, "r", encoding="utf-8") as input_file:
@@ -11,8 +13,48 @@ def load_data(filepath):
     return graph
 
 
+def floyd_warshall(graph):
+    edges = set(graph.keys())
+
+    distances = {}
+    precedessors = {}
+    for x, y in edges:
+        if x not in distances:
+            distances[x] = {}
+        distances[x][y] = graph[(x, y)]
+        distances[x][x] = 0
+        if x not in precedessors:
+            precedessors[x] = {}
+        precedessors[x][y] = x
+
+    nodes = list(map(lambda edge: edge[0], edges))
+
+    for middle in nodes:
+        for start in nodes:
+            if middle not in distances[start]:
+                distances[start][middle] = math.inf
+            for end in nodes:
+                if end not in distances[start]:
+                    distances[start][end] = math.inf
+
+                if end not in distances[middle]:
+                    distances[middle][end] = math.inf
+
+                if distances[start][end] > distances[start][middle] + distances[middle][end]:
+                    distances[start][end] = distances[start][middle] + distances[middle][end]
+                    precedessors[start][end] = precedessors[middle][end]
+
+    return distances, precedessors
+
 
 if __name__ == "__main__":
     file_name = "data.txt"
     data = load_data(file_name)
-    print(data)
+    d, p = floyd_warshall(data)
+    print("Distance(1,20) =", d[1][20])
+    x = 1
+    print("Path:")
+    while x != p[x][20]:
+        next_x = p[x][20]
+        print(x, " -> ", next_x)
+        x = next_x
