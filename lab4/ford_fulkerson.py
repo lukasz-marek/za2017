abd04816b88b8695f82609d6c5ea7cc42474a631
Lukasz Marek <marek.lucas@gmail.com>
2017-11-05 18:08:13 +0100

    More polished version.

--- lab4/ford_fulkerson.py
+++ lab4/ford_fulkerson.py
@@ -13,14 +13,17 @@ def load_data(file_name):
             if source not in graph:
                 graph[source] = {}
             graph[source][destination] = flow
+    return graph
 
+
+def transform_graph_into_matrix(graph):
     vertexes = get_vertexes(graph)
     matrix_size = max(vertexes) + 1
     graph_matrix = np.zeros((matrix_size, matrix_size))
     for v1, v2s in graph.items():
         for v2, flow in v2s.items():
             graph_matrix[v1][v2] = flow
-    return graph, graph_matrix
+    return graph_matrix
 
 
 def get_vertexes(graph):
@@ -72,7 +75,8 @@ def compute_path_flow(path, c, f):
     return cf
 
 
-def ford_fulkerson(graph, c, source, destination):
+def ford_fulkerson(graph, source, destination):
+    c = transform_graph_into_matrix(graph)
     f = np.zeros_like(c)
 
     vertexes = get_vertexes(graph)
@@ -84,20 +88,33 @@ def ford_fulkerson(graph, c, source, destination):
 
     path = next(paths_generator, None)
 
-    max_flow = 0
     while path is not None:
         cf = compute_path_flow(path, c, f)
-        max_flow += cf
         for index in range(len(path) - 1):
             u, v = path[index], path[index + 1]
             f[u][v] = f[u][v] + cf
             f[v][u] = f[v][u] - cf
         path = next(paths_generator, None)
 
-    return max_flow, f
+    return f
 
 
 if __name__ == "__main__":
-    graph, matrix = load_data("graph1.txt")
-    max_flow, flows = ford_fulkerson(graph, matrix, 43, 180)
-    print(max_flow)
+    graph = load_data("graph1.txt")
+    flows = ford_fulkerson(graph, 43, 180)
+    print("Flow 43->180 =", flows[43][180])
+
+    nodes = get_vertexes(graph)
+    test_source = 43
+    max_node = -1
+    max_value = -inf
+    for node in nodes:
+        if node != test_source:
+            flows = ford_fulkerson(graph, test_source, node)
+            flow = np.sum(np.array([x if x > 0 else 0 for x in flows[node]]))
+            if flow > max_value:
+                max_node = node
+
+    print("max flow = ", max_value, " achieved for node ", max_node)
+
+
