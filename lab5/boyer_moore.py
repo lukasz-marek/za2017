1ce1b141d4e3a39dbbd2a61c96328b2bab10df73
Lukasz Marek <marek.lucas@gmail.com>
2017-11-06 09:58:41 +0100

    naive approach

--- lab5/boyer_moore.py
+++ lab5/boyer_moore.py
@@ -7,50 +7,25 @@ def compute_last_occurrence(pattern, alphabet):
     return _lambda
 
 
-def compute_good_suffix(pattern):
-    prefix = compute_prefix(pattern)
-    inverse_prefix = compute_prefix(pattern[::-1])
-    good_suffix = {}
-
-    for j in range(len(pattern)):
-        good_suffix[j] = len(pattern) - prefix[len(pattern)]
-
-    for l in range(1, len(pattern)):
-        j = len(pattern) - inverse_prefix[l]
-        if good_suffix[j] > l - inverse_prefix[l]:
-            good_suffix[j] = l - inverse_prefix[l]
-
-    return good_suffix
-
-
-def compute_prefix(pattern):
-    prefix = {}
-    for k in range(len(pattern)):
-        for q in range(len(pattern)):
-            if k < q and pattern[:q].endswith(pattern[:k]):
-                if q in prefix:
-                    prefix[q] = k if k > prefix[q] else prefix[q]
-                else:
-                    prefix[q] = k
-    return prefix
-
-
 def boyer_moore(text, pattern):
     alphabet = [x for x in text]
     alphabet.extend([x for x in pattern])
     alphabet = set(alphabet)
     last = compute_last_occurrence(pattern, alphabet)
-    suffix = compute_good_suffix(pattern)
 
-    s = 0
-    while s <= len(text) - len(pattern):
-        j = len(pattern)
-        while j > 0 and pattern[j] == text[s + j]:
-            j = j - 1
-        if j == 0:
-            print(s)
+    i = len(text) - 1
+    j = len(pattern) - 1
+    while len(text) > i > 0:
+        if pattern[j] == text[i]:
+            if j == 0:
+                return i
+            else:
+                j -= 1
+                i -= 1
         else:
-            s += max(suffix[j], j - last[text[s + j]])
+            i = i -1 #+ len(pattern) - min(j, 1 + last[text[i]])
+            j = len(pattern) - 1
+    return -1
 
 
 def get_text(filename):
